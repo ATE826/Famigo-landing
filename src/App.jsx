b08d73ff667bbbdadf2e9dbecd3ffaf6d0d83ce3
ToniTsey <antontseysnovskiy@gmail.com>
2026-09-12 20:49:02 +0300

    Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import bg from "./assets/background.png";
+import logo from "./assets/Logo.png";
+import fridgeImg from "./assets/fridge.png";
+import budgetImg from "./assets/budget.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <header>
@@ -12,7 +14,7 @@ function App() {
             <div>
               <img
                 style={{ width: "35px", height: "35px" }}
-                src="src\assets\Logo.png"
+                src={logo}
                 alt="Famigo Logo"
               />
             </div>
@@ -27,42 +29,102 @@ function App() {
             <a href="#team">Команда</a>
           </nav>
           <a href="" className="btn-primary">
-            Создать семью
+            Добавить семью
           </a>
         </div>
       </header>
       <main>
         <section className="hero">
           <div className="bg">
-            <img
-              src="https://images.unsplash.com/photo-1758687126864-96b61e1b3af0?fm=jpg&amp;q=85&amp;w=2600&amp;auto=format&amp;fit=crop"
-              alt="Семья вместе дома"
-            ></img>
+            <img src={bg} alt="Семья вместе дома"></img>
             <div className="tone"></div>
           </div>
           <div className="hero-row">
             <div className="text-content">
-              {/* <div className="product-tags">
+              <div className="product-tags">
                 <p className="tag">Тёплый</p>
                 <p className="tag">Cпокойный</p>
                 <p className="tag">Технологичный</p>
-              </div> */}
-              <h1 className="text-primary">
+              </div>
+              <h1 className="text-primary hero-text-primary">
                 Хватит держать семью в десятке приложений
               </h1>
-              <p className="text-secondary">
+              <p className="text-secondary hero-text-secondary">
                 Famigo собирает бюджет, холодильник, календарь и общение в одном
                 тёплом пространстве — чтобы важное не терялось в чатах и
                 заметках.
               </p>
               <div className="button">
                 <a href="" className="btn-primary">
-                  Создать семью
+                  Добавть свою семью
                 </a>
-                <p className="text-tertiary">Всего за 2 минуты</p>
+                <p className="text-tertiary near-button-text">
+                  Всего за 2 минуты
+                </p>
               </div>
             </div>
-            <div className="card-content"></div>
+            <div className="card-content">
+              <div className="card">
+                <div className="card-row">
+                  <p className="text-primary card-category">Семья Ивановых</p>
+                  <p className="text-tertiary">4 человека</p>
+                </div>
+                <div className="card-row">
+                  <div className="card-icon-title">
+                    <p>💰</p>
+                    <p className="text-secondary card-category">Бюджет</p>
+                  </div>
+                  <p className="text-secondary card-category">
+                    <b>1 963 580 ₽</b>
+                  </p>
+                </div>
+                <div className="card-row">
+                  <div className="card-icon-title">
+                    <p>📅</p>
+                    <p className="text-secondary card-category">Календарь</p>
+                  </div>
+                  <p className="text-secondary card-category">
+                    <b>Семейный ужин</b>
+                  </p>
+                </div>
+                <div className="card-row last-card-category">
+                  <div className="card-icon-title">
+                    <p>🗄️</p>
+                    <p className="text-secondary card-category">Холодильник</p>
+                  </div>
+                  <p className="text-secondary card-category last-cartd-category">
+                    <b>Молоко заканчивается</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div className="statement">
+            <p className="text-primary text-statement">
+              <span style={{ color: "var(--dark)" }}>
+                Тёплая, спокойная и технологичная
+              </span>
+              семейная экосистема — всё необходимое без лишних деталей
+              ивизуальной перегрузки.
+            </p>
+          </div>
+          <div className="info">
+            <div className="info-section">
+              <div className="card-grid card-info">
+                <div className="card-image">
+                  <img src={fridgeImg} alt="Fridge image" />
+                </div>
+                <div className="content">sdfsfd</div>
+              </div>
+              <div className="card-grid card-info">
+                <div className="content">asdsadsa</div>
+                <div className="card-image">
+                  <img src={budgetImg} alt="Budget image" />
+                </div>
+              </div>
+            </div>
           </div>
         </section>
       </main>
